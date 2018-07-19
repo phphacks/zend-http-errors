2a094332d3f9f00b15ebcc4f88429651cf617805
Lucas A. de Araújo <lucas.araujo@bevicred.com.br>
2018-07-19 08:04:35 -0300

    Implementação da factory para tratamento dos forbiddens.

--- tests/Factory/ResponseFactoryTest.php
+++ tests/Factory/ResponseFactoryTest.php
@@ -2,32 +2,45 @@
 
 namespace Zend\HttpErrors\Tests\Factory;
 
+use Zend\HttpErrors\Exceptions\Base\HttpErrorException;
 use Zend\HttpErrors\Exceptions\HttpBadRequestException;
 use Zend\HttpErrors\Exceptions\HttpForbiddenException;
 use Zend\HttpErrors\Exceptions\HttpUnauthorizedException;
 use Zend\HttpErrors\Factory\ResponseFactory;
 use Zend\HttpErrors\Tests\ApplicationTestCase;
 
+/**
+ * ResponseFactoryTest
+ *
+ * @package Zend\HttpErrors\Tests\Factory
+ */
 class ResponseFactoryTest extends ApplicationTestCase
 {
+    /**
+     * @var HttpErrorException[]
+     */
     private $exceptions;
 
+    /**
+     * Setup
+     */
     public function setUp()
     {
         $this->exceptions = [
-            HttpBadRequestException::class,
-            HttpUnauthorizedException::class,
-            HttpForbiddenException::class
+            new HttpBadRequestException(),
+            new HttpUnauthorizedException(),
+            new HttpForbiddenException()
         ];
 
         parent::setUp();
     }
 
+    /**
+     * Exceptions
+     */
     public function testExceptions()
     {
         foreach ($this->exceptions as $exception) {
-            $exception = new $exception("Deu fita", 123);
-
             /** @var ResponseFactory $factory */
             $factory = $this->serviceManager->get('Zend\HttpErrors\Factory\ResponseFactory');
             $response = $factory->createFor($exception);
